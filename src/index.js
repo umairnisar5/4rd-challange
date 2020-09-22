ea79d5c67617614da615e42556bf699e9f7f5976
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-09-22 23:41:27 +0500

    use React add componant

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {add, sub , mult , div } from "./Cals";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+
+  <ul>
+    <li>The sum of two no is  {add(40,4)}</li>
+    <li>The subtraction of   {sub (40,4)} </li>
+    <li>The multipliction of  {mult (40,4)} </li>
+    <li>The divion of  {div (40,4)} </li>
+
+  </ul>
+
+  </>,
+ 
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
